c7c4b7a0b9e3a84d2d622daa4a8d636bc163d0bc
Apo <jiyuny@yahoo.fr>
2014-04-02 23:57:55 +0200

    Merge branch 'master' of https://github.com/apo-j/PMAC
    Conflicts:
    	db/schema.rb

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require jquery-1.10.2.min
+//= require easyzoom.js
 //= require jquery.elevatezoom.js
 //= require bootstrap
 //= require javascript-extensions
