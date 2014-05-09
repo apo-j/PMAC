87132b954aee11d54a1ada5ee0b7cd32252f139f
Apo <jiyuny@yahoo.fr>
2014-05-09 22:44:51 +0200

    New changelist

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325211130) do
+ActiveRecord::Schema.define(version: 20140505195736) do
 
   create_table "addresses", force: true do |t|
     t.string   "name"
@@ -142,7 +142,10 @@ ActiveRecord::Schema.define(version: 20140325211130) do
     t.string   "pay_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "state"
   end
+
+  add_index "orders", ["state"], name: "idx_state", using: :btree
 
   create_table "people", force: true do |t|
     t.string   "first_name"
